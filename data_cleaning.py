bd7a883d0a3ce3bed12f91291493c10568f35f07
ijaz-hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-02-14 22:08:03 -0800

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
+# Reading original data file
 df = pd.read_csv('covid-variants.csv')
-df.drop(df.index[~df.variant.isin(['Alpha', 'Beta', 'Delta', 'Omicron', 'Mu'])])
-df.drop(columns=df.columns[0], axis=1, inplace=True)
+
+# Dropping all rows except where variants are Alpha, Beta, Delta, Omicron, or Mu
+df = df.drop(df.index[~df.variant.isin(['Alpha', 'Beta', 'Delta', 'Omicron', 'Mu'])])
+
+# Restarting index numbering
+df = df.reset_index(drop=True)
+
+# Writing to new cleaned data file
 df.to_csv('covid-variants-cleaned.csv')
