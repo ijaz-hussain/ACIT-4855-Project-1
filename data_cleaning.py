83341fc18946363929aab59a69d42e12b1930876
Ijaz Hussain <62718643+ijaz-hussain@users.noreply.github.com>
2022-02-17 23:25:38 -0800

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('covid-variants.csv')
 
 # Removes negative percentage (-0.00)
-df.drop(df[(df.perc_sequences < 0)].index, inplace=True)
+df = df.drop(df[(df.perc_sequences < 0)].index, inplace=True)
 
 # Dropping all rows except where variants are Alpha, Beta, Delta, Omicron, or Mu
 df = df.drop(df.index[~df.variant.isin(['Alpha', 'Beta', 'Delta', 'Omicron', 'Mu'])])
